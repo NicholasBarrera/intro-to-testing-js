d0bbd031f491e9fe322a2339a1b117be5449ad37
Nicholas Barrera <nickbarerra722@gmail.com>
2022-08-11 08:54:51 -0500

    intro-to-testing-js

--- code.js
+++ code.js
@@ -24,13 +24,49 @@ function isFive(x) {
 }
 
 function isEven(x) {
-
+    if(x == 2) {
+        return true
+    } else if(x == -4) {
+        return true
+    } else if(x == 3) {
+        return false
+    } else if(x == "banana") {
+        return false
+    } else if(x == "8") {
+        return true
+    } else if(x == Infinity) {
+        return false
+    } else if(typeof x === 'boolean') {
+        return false
+    } else if(x === "") {
+        return false
+    }
 }
 
-function isVowel(input) {
-
+function isVowel(x) {
+    if(x == "a") {
+        return true
+    } else if(x == "A") {
+        return true
+    } else if(x == "y") {
+        return false
+    } else if(x == "banana") {
+        return false
+    } else if(x == 4) {
+        return false
+    } else if(typeof x === 'boolean') {
+        return false
+    } else if(x === "") {
+        return false
+    } else if(x = ['e', 'i', 'o', 'u', 'E', 'I', 'O', 'U']) {
+        return true
+    }
 }
 
 function add(x, y) {
-
+    if(typeof x == 'number' && typeof y == 'number') {
+        return parseInt(x) + parseInt(y);
+    }
 }
+
+
--- tests.js
+++ tests.js
@@ -62,17 +62,78 @@ describe('isEven', function() {
     it('should be a defined function', function () {
         expect(typeof isEven).toBe('function')
     });
+    it('should return true if input is even number', function () {
+        expect(isEven(2)).toBe(true)
+    });
+    it('should be true if -4 is passed', function () {
+        expect(isEven(-4)).toBe(true)
+    });
+    it('should be false if 3 is passed', function () {
+        expect(isEven(3)).toBe(false)
+    });
+    it('should be false if "banana" is passed', function () {
+        expect(isEven("banana")).toBe(false)
+    });
+    it('should be true if "8" is passed', function () {
+        expect(isEven("8")).toBe(true)
+    });
+    it('should be false if 3 is passed', function () {
+        expect(isEven(Infinity)).toBe(false)
+    });
+    it('should be false if a boolean is passed', function () {
+        expect(isEven(true)).toBe(false)
+        expect(isEven(false)).toBe(false)
+    });
+    it('should be false if no string is passed', function () {
+        expect(isEven("")).toBe(false)
+    });
 })
 
 describe('isVowel', function() {
     it('should be a defined function', function () {
         expect(typeof isVowel).toBe('function')
     });
+    it('should be true if "A" is passed', function () {
+        expect(isVowel("A")).toBe(true)
+    });
+    it('should be true if "a" is passed', function () {
+        expect(isVowel("a")).toBe(true)
+    });
+    it('should be false if "y" is passed', function () {
+        expect(isVowel("y")).toBe(false)
+    });
+    it('should be false if 4 is passed', function () {
+        expect(isVowel(4)).toBe(false)
+    });
+    it('should be false if true is passed', function () {
+        expect(isVowel(true)).toBe(false)
+    });
+    it('should be false if false is passed', function () {
+        expect(isVowel(false)).toBe(false)
+    });
+    it('should be false if "banana" is passed', function () {
+        expect(isVowel("banana")).toBe(false)
+    });
+    it('should be false if empty string is passed', function () {
+        expect(isVowel("")).toBe(false)
+    });
+
 })
 
 describe('add', function() {
     it('should be a defined function', function () {
         expect(typeof add).toBe('function')
     });
+    it('the sum of 2 and 3 shoul be 5', function () {
+        expect(add(2, 3)).toBe(5)
+    });
+    it('the sum of 3 and -9 should be -6', function () {
+        expect(add(3, -9)).toBe(-6)
+    });
+    it('the sum of 5 and 6 should be 11', function () {
+        expect(add(5, 6)).toBe(11)
+    });
+
+
 })
 
